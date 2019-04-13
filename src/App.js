61c849092057843395650c57e1f7fc87d1456c4e
Alexander Römelt <roemelt@stud.uni-heidelberg.de>
2019-04-13 09:15:02 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Widget } from 'rasa-webchat';
+import theme from './Widget.css'
+
+class Header extends Component {
+	render() {
+		return (
+			<div className="Header">
+				<p>-Ethicbot-</p>
+			</div>
+		);
+	}
+}
+
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<Header />
+				<Widget 
+					initPayload={"/greeting"}
+	      			socketUrl={"http://localhost:5005"}
+	      			socketPath={"/socket.io/"}
+	      			embedded={true}
+	      			params={{storage: "session"}}
+	      			theme={theme}
+				/>
+			</div>
+      	);
+	}
+}
+
+export default App;
